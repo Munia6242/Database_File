ca8ef09097a60629ea949c7a575e818742f514c3
Munia6242 <makrumit.munia@gmail.com>
2025-05-26 12:01:36 +0600

    Database Work

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+?>
+
+<table>
+    <thead>
+        <tr>
+            <th>Name</th>
+            <th>Father's Name</th>
+            <th>Mother's Name</th>
+            <th>Age</th>
+            <th>Address</th>
+            <th>Phone</th>
+            <th>Image</th>
+        </tr>
+    </thead>
+</table>
